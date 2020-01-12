79f8ecec30041a66b8ab26ef929355c725ac94aa
Thanh Quy Vo <vtquy98@gmail.com>
2020-01-12 14:35:47 +0700

    final test: call contract successful

--- server/graphql/schema/voting/query.resolver.js
+++ server/graphql/schema/voting/query.resolver.js
@@ -1,16 +1,41 @@
-import { Users } from '../../../services';
+// import { Users } from '../../../services';
+// import { combineResolvers } from 'graphql-resolvers';
+// import { checkAuthentication } from '../../libs';
+// import { path } from 'lodash/fp';
 
-import { combineResolvers } from 'graphql-resolvers';
-import { checkAuthentication } from '../../libs';
-
-import { path } from 'lodash/fp';
-import { default as Web3 } from 'web3';
-import { default as contract } from 'truffle-contract';
+// import { default as Web3 } from 'web3';
+// import { default as contract } from 'truffle-contract';
 import voting_artifacts from '../../../build/contracts/Voting.json';
+var contract = require('@truffle/contract');
+// const Voting = contract(voting_artifacts);
 
-const Voting = contract(voting_artifacts);
-const web3 = new Web3(Web3.givenProvider || 'ws://localhost:8545');
+var Web3 = require('web3');
+var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 
+const abi = voting_artifacts.abi;
+
+const MyTruffleContract = contract({
+  abi: abi
+});
+
+MyTruffleContract.setProvider(web3.currentProvider);
+
+MyTruffleContract.at('0x1A98A4619a3a90c7c40F9Fc4610DEBAB0b8f58C8').then(
+  instance => {
+    // instance.allCandidates().then(candidates => console.log(candidates));
+    instance
+      .voteForCandidate(
+        '0x4e69636b00000000000000000000000000000000000000000000000000000000',
+        { from: '0x47A8D0AD555fc02311778f3b437cdE1732B8290B' }
+      )
+      .then(v => {
+        console.log(v);
+      });
+  }
+);
+
+// console.log(web3.eth.getAccounts();
+//
 module.exports = {
   Query: {
     // get_voting_user: combineResolvers(
@@ -19,25 +44,8 @@ module.exports = {
     //     return Users.findOne({ id: currentUser.id });
     //   }
     // ),
-    get_voting_data: async (_, { __ }) => {
-      //   return Categories.findOne({ });
-      //   web3.eth.getAccounts().then(console.log);
-
-      console.log(voting_artifacts[Object.keys(voting_artifacts)[12]);
-      //   console.log(Voting.options.address);
-
-      //   web3.eth.net
-      //     .isListening()
-      //     .then(s => {
-      //       console.log("We're still connected to the node");
-      //       console.log(web3.currentProvider);
-      //     })
-      //     .catch(e => {
-      //       console.log('Lost connection to the node, reconnecting');
-      //       web3.setProvider(
-      //         new web3.providers.HttpProvider('http://localhost:8545')
-      //       );
-      //     });
-    }
+    // get_voting_data: async (_, { __ }) => {
+    //   // console.log(MyTruffleContract);
+    // }
   }
 };
