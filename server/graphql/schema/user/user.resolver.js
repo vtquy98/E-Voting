c45f4ed927ecc2e5706f35e3bf0e2411312a68b0
Thanh Quy Vo <vtquy98@gmail.com>
2020-01-30 21:30:07 +0700

    feat and refactor:  - have done user schema  - migrate init system account  - Login feature  - remove something unuse

--- server/graphql/schema/user/user.resolver.js
+++ server/graphql/schema/user/user.resolver.js
@@ -1,8 +1,21 @@
 import { path } from 'lodash/fp';
+import { USER, ADMIN } from '../../../enums/userRole';
+import { saveSession } from '../../../middlewares/session';
+import auth from '../../../authentication';
 
 module.exports = {
   User: {
     googleId: path('google_id'),
-    walletAddress: path('wallet_address')
+    walletAddress: path('wallet_address'),
+    fullName: path('full_name'),
+    token: (user, _, { req }) => {
+      const jwt = auth.sign(user);
+      saveSession(req.session, jwt);
+      return 'bearer ' + jwt;
+    }
+  },
+  Role: {
+    ADMIN,
+    USER
   }
 };
--- server/services/user.js
+++ server/services/user.js
@@ -1,12 +1,12 @@
 import Users from '../models/user';
-import { USER_ACTIVE } from '../enums/userStatus';
+// import { USER_ACTIVE } from '../enums/userStatus';
 
 export const getUser = payload => {
   if (!payload) {
     return null;
   }
   //status: USER_ACTIVE add later!
-  return Users.findOne({ googleId: payload.googleId });
+  return Users.findOne({ id: payload.id });
 };
 
 module.exports = Users;
