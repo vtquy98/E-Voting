caaf12a1fd0feec91648c3cdd287d2091b6616b7
Thanh Quy Vo <vtquy98@gmail.com>
2020-01-30 21:30:07 +0700

    feat and refactor:  - have done user schema  - migrate init system account  - Login feature  - remove something unuse

--- server/migrations/1577871275635-init-project.js
+++ server/migrations/1577871275635-init-project.js
@@ -1,57 +1,75 @@
-// import mongoose from 'mongoose';
-// import { Users } from '../services';
-// import { MALE } from '../enums/userGender';
-// import { USER_ACTIVE } from '../enums/userStatus';
-// import { USER, ADMIN } from '../enums/userRole';
+import mongoose from 'mongoose';
+import { ADMIN, USER } from '../enums/userRole';
+import { Users } from '../services';
 
-// const systemUser = {
-//   username: 'admin',
-//   password: '123456',
-//   first_name: 'Young Radio',
-//   last_name: 'Admin',
-//   email: 'youngradio.admin@gmail.com',
-//   gender: MALE,
-//   birth_date: '19/10/1998',
-//   quote: 'Death is like a wind, always by my side.',
-//   status: USER_ACTIVE,
-//   role: ADMIN,
-//   profession: 'Student'
-// };
+const {
+  MONGODB_URI,
+  MONGO_DB,
+  MONGO_OPTIONS,
+  ADMIN_WALLET_ADDRESS,
+  USER_WALLET_ADDRES
+} = process.env;
 
-// const insertSystemUser = () => new Users(systemUser).save();
+const adminWalletAddress =
+  ADMIN_WALLET_ADDRESS || '0x001526F8bF8A346abF4d2d60B7e5BA4BeC75FB28';
 
-// const { MONGODB_URI, MONGO_DB, MONGO_OPTIONS } = process.env;
+const mockUserWalletAddress =
+  USER_WALLET_ADDRES || '0x8a0Aa7Fd4fC557dBF8d97291c520Fa7a6E64F696';
 
-// const mongoUrl = MONGODB_URI || 'mongodb://localhost:27017';
-// const dbName = MONGO_DB || 'E-Voting';
-// const option = MONGO_OPTIONS
-//   ? JSON.parse(MONGODB_URI)
-//   : {
-//       useNewUrlParser: true
-//     };
-
-// const connectDb = () => {
-//   return new Promise((resolve, reject) => {
-//     mongoose.connect(mongoUrl + '/' + dbName, option, err => {
-//       if (err) {
-//         reject(err);
-//       }
-//       resolve(mongoose);
-//     });
-//   });
-// };
-
-module.exports.up = async function() {
-  // const db = await connectDb();
-  // await insertSystemUser();
-  // await db.disconnect();
-  console.log('DO LATER');
+const systemUser = {
+  username: 'admin',
+  password: '123456',
+  full_name: 'AGU E-VOTING',
+  email: 'agu-evoting.admin@gmail.com',
+  wallet_address: adminWalletAddress,
+  role: ADMIN,
+  google_id: 'undefined'
 };
 
-// module.exports.down = async function() {
-//   const db = await connectDb();
+const mockUser = {
+  username: 'user',
+  password: '123456',
+  full_name: 'AGU E-VOTING',
+  email: 'agu-evoting.user@gmail.com',
+  wallet_address: mockUserWalletAddress,
+  role: USER,
+  google_id: 'undefined'
+};
 
-//   await Promise.all([Users.deleteOne({ username: systemUser.username })]);
+const insertSystemUser = () => new Users(systemUser).save();
+const insertMockUser = () => new Users(mockUser).save();
 
-//   await db.disconnect();
-// };
+const mongoUrl = MONGODB_URI || 'mongodb://localhost:27017';
+const dbName = MONGO_DB || 'E-Voting';
+const option = MONGO_OPTIONS
+  ? JSON.parse(MONGODB_URI)
+  : {
+      useNewUrlParser: true
+    };
+
+const connectDb = () => {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(mongoUrl + '/' + dbName, option, err => {
+      if (err) {
+        reject(err);
+      }
+      resolve(mongoose);
+    });
+  });
+};
+
+module.exports.up = async function() {
+  const db = await connectDb();
+  await insertSystemUser();
+  await insertMockUser();
+  await db.disconnect();
+};
+
+module.exports.down = async function() {
+  const db = await connectDb();
+  await Promise.all([
+    Users.deleteOne({ username: systemUser.username }),
+    Users.deleteOne({ username: mockUser.username })
+  ]);
+  await db.disconnect();
+};
