101c801c3a721a7c7932fdd0956e3998d8506685
Thanh Quy Vo <vtquy98@gmail.com>
2020-02-22 12:33:09 +0700

    hotfix: update env and refactor to deploy

--- server/graphql/libs/electionCreation.js
+++ server/graphql/libs/electionCreation.js
@@ -4,9 +4,10 @@ import ElectionCreation from '../../contract-build/ElectionCreation.json';
 
 //how to get contract address on db!!!??
 //this is unique deployed contract!
+
 const instance = new web3.eth.Contract(
   ElectionCreation.abi,
-  '0x8dB6255Ac519fE817c50eCC64512ae2FF1cC49b4'
+  process.env.CONTRACT_ADDRESS
 );
 
 export default instance;
