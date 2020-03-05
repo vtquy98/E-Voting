f9cd1e01961a3fbb32ff0b1a55c781d7559c27c9
Thanh Quy Vo <vtquy98@gmail.com>
2020-03-05 12:48:36 +0700

    Feat: home landing page

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -10,184 +10,274 @@ export default class MyDocument extends Document {
 
   render() {
     const rootProps = this.props;
+    const path = this.props.__NEXT_DATA__.page;
 
     return (
       <html>
-        <Head>
-          <meta charSet="UTF-8" />
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
-          <meta name="mobile-web-app-capable" content="yes" />
-          <meta name="apple-mobile-web-app-title" content="React-Voting" />
-          <meta name="apple-mobile-web-app-status-bar-style" />
-          <meta name="apple-mobile-web-app-capable" content="yes" />
-          <meta name="application-name" content="QR Scanner" />
-          <meta name="msapplication-TileColor" />
+        {path === '/' ? (
+          <React.Fragment>
+            <Head>
+              <link
+                rel="stylesheet"
+                href="/static/assets-homepage/css/style.css"
+              />
 
-          {/* test */}
+              <link
+                rel="shortcut icon"
+                href="/static/assets-homepage/images/favicon.png"
+                type="image/png"
+              />
 
-          <link
-            href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i%7COpen+Sans:300,300i,400,400i,600,600i,700,700i"
-            rel="stylesheet"
-          />
-          <link
-            rel="stylesheet"
-            type="text/css"
-            href="/static/assets/css/vendors.css"
-          />
-          <link
-            rel="stylesheet"
-            type="text/css"
-            href="/static/assets/vendors/css/forms/icheck/icheck.css"
-          />
-          <link
-            rel="stylesheet"
-            type="text/css"
-            href="/static/assets/vendors/css/forms/icheck/custom.css"
-          />
-          <link
-            rel="stylesheet"
-            type="text/css"
-            href="/static/assets/vendors/css/charts/morris.css"
-          />
-          <link
-            rel="stylesheet"
-            type="text/css"
-            href="/static/assets/vendors/css/extensions/unslider.css"
-          />
-          <link
-            rel="stylesheet"
-            type="text/css"
-            href="/static/assets/vendors/css/weather-icons/climacons.min.css"
-          />
-          <link
-            rel="stylesheet"
-            type="text/css"
-            href="/static/assets/css/app.css"
-          />
-          <link
-            rel="stylesheet"
-            type="text/css"
-            href="/static/assets/css/core/menu/menu-types/vertical-menu.css"
-          />
-          <link
-            rel="stylesheet"
-            type="text/css"
-            href="/static/assets/css/core/colors/palette-climacon.css"
-          />
-          <link
-            rel="stylesheet"
-            type="text/css"
-            href="/static/assets/css/core/colors/palette-gradient.css"
-          />
-          <link
-            rel="stylesheet"
-            type="text/css"
-            href="/static/assets/fonts/simple-line-icons/style.min.css"
-          />
-          <link
-            rel="stylesheet"
-            type="text/css"
-            href="/static/assets/fonts/meteocons/style.min.css"
-          />
-          <link
-            rel="stylesheet"
-            type="text/css"
-            href="/static/assets/css/pages/users.css"
-          />
+              <link
+                rel="stylesheet"
+                href="/static/assets-homepage/css/slick.css"
+              />
 
-          <link
-            rel="stylesheet"
-            type="text/css"
-            href="/static/assets/css/pages/project.css"
-          />
+              <link
+                rel="stylesheet"
+                href="/static/assets-homepage/css/font-awesome.min.css"
+              />
 
-          {/* custom css */}
-          <link
-            rel="stylesheet"
-            type="text/css"
-            href="/static/assets/css/custom.css"
-          />
-          <link
-            rel="stylesheet"
-            type="text/css"
-            href="/static/assets/css/nprogress.css"
-          />
+              <link
+                rel="stylesheet"
+                href="/static/assets-homepage/css/LineIcons.css"
+              />
 
-          {/* end custom */}
-        </Head>
-        <body
-          className="vertical-layout vertical-menu 2-columns   menu-expanded fixed-navbar"
-          data-open="click"
-          data-menu="vertical-menu"
-          data-col="2-columns"
-        >
-          <Main {...rootProps} />
-          <NextScript />
+              <link
+                rel="stylesheet"
+                href="/static/assets-homepage/css/animate.css"
+              />
 
-          <script
-            src="/static/assets/vendors/js/vendors.min.js"
-            type="text/javascript"
-          ></script>
+              <link
+                rel="stylesheet"
+                href="/static/assets-homepage/css/magnific-popup.css"
+              />
 
-          <script
-            src="//maps.googleapis.com/maps/api/js?key=AIzaSyBDkKetQwosod2SZ7ZGCpxuJdxY3kxo5Po"
-            type="text/javascript"
-          ></script>
-          <script
-            src="/static/assets/vendors/js/charts/gmaps.min.js"
-            type="text/javascript"
-          ></script>
-          <script
-            src="/static/assets/vendors/js/forms/icheck/icheck.min.js"
-            type="text/javascript"
-          ></script>
-          <script
-            src="/static/assets/vendors/js/extensions/jquery.knob.min.js"
-            type="text/javascript"
-          ></script>
-          <script
-            src="/static/assets/vendors/js/charts/raphael-min.js"
-            type="text/javascript"
-          ></script>
-          <script
-            src="/static/assets/vendors/js/charts/morris.min.js"
-            type="text/javascript"
-          ></script>
-          <script
-            src="/static/assets/vendors/js/charts/jquery.sparkline.min.js"
-            type="text/javascript"
-          ></script>
-          <script
-            src="/static/assets/vendors/js/extensions/unslider-min.js"
-            type="text/javascript"
-          ></script>
-          <script
-            src="/static/assets/vendors/js/charts/echarts/echarts.js"
-            type="text/javascript"
-          ></script>
+              <link
+                rel="stylesheet"
+                href="/static/assets-homepage/css/bootstrap.min.css"
+              />
 
-          <script
-            src="/static/assets/js/core/app-menu.js"
-            type="text/javascript"
-          ></script>
-          <script
-            src="/static/assets/js/core/app.js"
-            type="text/javascript"
-          ></script>
-          <script
-            src="/static/assets/js/scripts/customizer.js"
-            type="text/javascript"
-          ></script>
+              <link
+                rel="stylesheet"
+                href="/static/assets-homepage/css/default.css"
+              />
+            </Head>
 
-          <script
-            src="/static/assets/js/scripts/pages/dashboard-fitness.js"
-            type="text/javascript"
-          ></script>
-        </body>
+            <body>
+              <Main {...rootProps} />
+              <NextScript />
+
+              <script src="/static/assets-homepage/js/vendor/jquery-1.12.4.min.js" />
+              <script src="/static/assets-homepage/js/vendor/modernizr-3.7.1.min.js" />
+
+              <script src="/static/assets-homepage/js/popper.min.js" />
+              <script src="/static/assets-homepage/js/bootstrap.min.js" />
+
+              <script src="/static/assets-homepage/js/imagesloaded.pkgd.min.js" />
+              <script src="/static/assets-homepage/js/isotope.pkgd.min.js" />
+
+              <script src="/static/assets-homepage/js/waypoints.min.js" />
+              <script src="/static/assets-homepage/js/jquery.counterup.min.js" />
+
+              <script src="/static/assets-homepage/js/circles.min.js" />
+
+              <script src="/static/assets-homepage/js/jquery.appear.min.js" />
+
+              <script src="/static/assets-homepage/js/wow.min.js" />
+
+              <script src="/static/assets-homepage/js/headroom.min.js" />
+
+              <script src="/static/assets-homepage/js/jquery.nav.js" />
+
+              <script src="/static/assets-homepage/js/scrollIt.min.js" />
+
+              <script src="/static/assets-homepage/js/jquery.magnific-popup.min.js" />
+
+              <script src="/static/assets-homepage/js/slick.min.js" />
+
+              <script src="/static/assets-homepage/js/main.js" />
+            </body>
+          </React.Fragment>
+        ) : (
+          <React.Fragment>
+            <Head>
+              <meta charSet="UTF-8" />
+              <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+              <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1.0"
+              />
+              <meta name="mobile-web-app-capable" content="yes" />
+              <meta name="apple-mobile-web-app-title" content="React-Voting" />
+              <meta name="apple-mobile-web-app-status-bar-style" />
+              <meta name="apple-mobile-web-app-capable" content="yes" />
+              <meta name="application-name" content="QR Scanner" />
+              <meta name="msapplication-TileColor" />
+
+              {/* test */}
+
+              <link
+                href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i%7COpen+Sans:300,300i,400,400i,600,600i,700,700i"
+                rel="stylesheet"
+              />
+              <link
+                rel="stylesheet"
+                type="text/css"
+                href="/static/assets/css/vendors.css"
+              />
+              <link
+                rel="stylesheet"
+                type="text/css"
+                href="/static/assets/vendors/css/forms/icheck/icheck.css"
+              />
+              <link
+                rel="stylesheet"
+                type="text/css"
+                href="/static/assets/vendors/css/forms/icheck/custom.css"
+              />
+              <link
+                rel="stylesheet"
+                type="text/css"
+                href="/static/assets/vendors/css/charts/morris.css"
+              />
+              <link
+                rel="stylesheet"
+                type="text/css"
+                href="/static/assets/vendors/css/extensions/unslider.css"
+              />
+              <link
+                rel="stylesheet"
+                type="text/css"
+                href="/static/assets/vendors/css/weather-icons/climacons.min.css"
+              />
+              <link
+                rel="stylesheet"
+                type="text/css"
+                href="/static/assets/css/app.css"
+              />
+              <link
+                rel="stylesheet"
+                type="text/css"
+                href="/static/assets/css/core/menu/menu-types/vertical-menu.css"
+              />
+              <link
+                rel="stylesheet"
+                type="text/css"
+                href="/static/assets/css/core/colors/palette-climacon.css"
+              />
+              <link
+                rel="stylesheet"
+                type="text/css"
+                href="/static/assets/css/core/colors/palette-gradient.css"
+              />
+              <link
+                rel="stylesheet"
+                type="text/css"
+                href="/static/assets/fonts/simple-line-icons/style.min.css"
+              />
+              <link
+                rel="stylesheet"
+                type="text/css"
+                href="/static/assets/fonts/meteocons/style.min.css"
+              />
+              <link
+                rel="stylesheet"
+                type="text/css"
+                href="/static/assets/css/pages/users.css"
+              />
+
+              <link
+                rel="stylesheet"
+                type="text/css"
+                href="/static/assets/css/pages/project.css"
+              />
+
+              {/* custom css */}
+              <link
+                rel="stylesheet"
+                type="text/css"
+                href="/static/assets/css/custom.css"
+              />
+              <link
+                rel="stylesheet"
+                type="text/css"
+                href="/static/assets/css/nprogress.css"
+              />
+
+              {/* end custom */}
+            </Head>
+            <body
+              className="vertical-layout vertical-menu 2-columns   menu-expanded fixed-navbar"
+              data-open="click"
+              data-menu="vertical-menu"
+              data-col="2-columns"
+            >
+              <Main {...rootProps} />
+              <NextScript />
+
+              <script
+                src="/static/assets/vendors/js/vendors.min.js"
+                type="text/javascript"
+              ></script>
+
+              <script
+                src="//maps.googleapis.com/maps/api/js?key=AIzaSyBDkKetQwosod2SZ7ZGCpxuJdxY3kxo5Po"
+                type="text/javascript"
+              ></script>
+              <script
+                src="/static/assets/vendors/js/charts/gmaps.min.js"
+                type="text/javascript"
+              ></script>
+              <script
+                src="/static/assets/vendors/js/forms/icheck/icheck.min.js"
+                type="text/javascript"
+              ></script>
+              <script
+                src="/static/assets/vendors/js/extensions/jquery.knob.min.js"
+                type="text/javascript"
+              ></script>
+              <script
+                src="/static/assets/vendors/js/charts/raphael-min.js"
+                type="text/javascript"
+              ></script>
+              <script
+                src="/static/assets/vendors/js/charts/morris.min.js"
+                type="text/javascript"
+              ></script>
+              <script
+                src="/static/assets/vendors/js/charts/jquery.sparkline.min.js"
+                type="text/javascript"
+              ></script>
+              <script
+                src="/static/assets/vendors/js/extensions/unslider-min.js"
+                type="text/javascript"
+              ></script>
+              <script
+                src="/static/assets/vendors/js/charts/echarts/echarts.js"
+                type="text/javascript"
+              ></script>
+
+              <script
+                src="/static/assets/js/core/app-menu.js"
+                type="text/javascript"
+              ></script>
+              <script
+                src="/static/assets/js/core/app.js"
+                type="text/javascript"
+              ></script>
+              <script
+                src="/static/assets/js/scripts/customizer.js"
+                type="text/javascript"
+              ></script>
+
+              <script
+                src="/static/assets/js/scripts/pages/dashboard-fitness.js"
+                type="text/javascript"
+              ></script>
+            </body>
+          </React.Fragment>
+        )}
       </html>
     );
   }
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import HomepageComponent from '../components/HomepageComponent';
-import EmptyPageLayout from '../layouts/EmptyPageLayout';
-const BlogsPage = rootProps => (
-  <EmptyPageLayout {...rootProps} title="not config">
-    <HomepageComponent />
-  </EmptyPageLayout>
-);
 
-export default BlogsPage;
+const HomePage = () => <HomepageComponent />;
+
+export default HomePage;
