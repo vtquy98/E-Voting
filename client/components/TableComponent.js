5784799efd4abf5cad75882e0b179ad31147b264
Thanh Quy Vo <vtquy98@gmail.com>
2020-04-19 14:10:08 +0700

    temp

---
+++ client/components/TableComponent.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { useTable, useSortBy } from 'react-table';
+import styled from 'styled-components';
+
+const Styles = styled.div`
+  thead tr th {
+    font-weight: bold;
+    font-size: 16px;
+    border-top: none;
+  }
+  td {
+    font-size: 16px;
+  }
+`;
+
+const TableComponent = ({ columns, data }) => {
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow
+  } = useTable(
+    {
+      columns,
+      data
+    },
+    useSortBy
+  );
+
+  return (
+    <Styles>
+      <table {...getTableProps()} className="table">
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                  {column.render('Header')}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row, i) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </Styles>
+  );
+};
+export default TableComponent;
