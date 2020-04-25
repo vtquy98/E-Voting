8264e063e788e51fce39f0797d625c53a77a8bb1
Thanh Quy Vo <vtquy98@gmail.com>
2020-04-25 23:34:07 +0700

    ref: user management

--- client/components/AddUserPopup.js
+++ client/components/AddUserPopup.js
@@ -46,93 +46,106 @@ class AddUserPopup extends React.Component {
       addUsers
     } = this.props;
     return (
-      <div className="card">
-        <div className="card-header">
-          <h4 className="card-title" id="basic-layout-form-center">
-            <i className="fa fa-plus-circle"></i> Add new user
-          </h4>
-        </div>
-        <div className="card-content">
+      <React.Fragment>
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="/static/assets/custom/tags-input.css"
+        />
+        <div className="card shadow border-none">
+          <div className="card-header py-3 text-center">
+            <h6 className="m-0 font-weight-bold text-primary ">
+              <i className="fa fa-plus-circle"></i> Add new user
+            </h6>
+          </div>
           <div className="card-body">
-            {successMessage ? (
-              <div>
-                <div className="d-flex justify-content-center">
-                  <img
-                    src="/static/assets/images/vote-success.svg"
-                    alt="success"
-                    className="height-150"
-                  />
-                </div>
-                <h3 className="text-center mt-2">Invite successfully!</h3>
-                <p className="text-center mt-2">
-                  They will review an email include
-                  <code>Username, password</code> to using E-Voting system
-                </p>
-
-                <div className="d-flex justify-content-center">
-                  <button
-                    type="button"
-                    className="btn btn-primary mt-2"
-                    onClick={onClick}
-                  >
-                    <i className="ft-x"></i> Close
-                  </button>
-                </div>
-              </div>
-            ) : (
-              <div>
-                <div className="card-text">
-                  <p>
-                    You are going to <code>Add new user</code>. They will
-                    receive an invitation mail to login. After adding user, you
-                    can add them into any election.
+            <div className="card-body">
+              {successMessage ? (
+                <div>
+                  <div className="d-flex justify-content-center">
+                    <img
+                      src="/static/assets/images/vote-success.svg"
+                      alt="success"
+                      width="50%"
+                    />
+                  </div>
+                  <h3 className="text-center mt-2">Invite successfully!</h3>
+                  <p className="text-center mt-2">
+                    They will review an email include
+                    <code> Username, password</code> to using E-Voting system
                   </p>
-                  <p>
-                    Type user's <code>AGU email</code> then press{' '}
-                    <code>enter</code>.
-                  </p>
-                </div>
 
-                <div className="form-actions center">
-                  <TagsInput
-                    className="form-control"
-                    value={listUserEmails}
-                    onChange={email => setEmail(email)}
-                  />
-                  <div className="mt-2 text-right">
-                    <button
-                      type="submit"
-                      className="btn btn-primary mr-1"
-                      onClick={e => {
-                        e.preventDefault();
-                        addUsers(listUserEmails);
-                      }}
-                    >
-                      <i className="fa fa-check-square-o"></i> Save
-                    </button>
+                  <div className="d-flex justify-content-center">
                     <button
                       type="button"
-                      className="btn btn-warning"
+                      className="btn btn-primary mt-2"
                       onClick={onClick}
                     >
-                      <i className="ft-x"></i> Cancel
+                      <i className="ft-x"></i> Close
                     </button>
                   </div>
                 </div>
-                {errorMessage ? (
-                  <div
-                    className="mt-2 alert alert-danger border-0 mb-2"
-                    role="alert"
-                  >
-                    <strong>Error: </strong>
-                    {errorMessage}
+              ) : (
+                <div>
+                  <div className="card-text">
+                    <p>
+                      You are going to <code>Add new user</code>. They will
+                      receive an invitation mail to login. After adding user,
+                      you can add them into any election.
+                    </p>
+                    <p>
+                      Type each <code>user's email</code> then press{' '}
+                      <code>enter</code>.
+                    </p>
                   </div>
-                ) : null}
-              </div>
-            )}
+
+                  <div className="form-actions center">
+                    <TagsInput
+                      className="form-control form-control-custom-2"
+                      value={listUserEmails}
+                      onChange={email => setEmail(email)}
+                    />
+                    <div className="mt-2 text-right">
+                      <button
+                        type="submit"
+                        className="btn btn-primary mr-1"
+                        onClick={e => {
+                          e.preventDefault();
+                          addUsers(listUserEmails);
+                        }}
+                      >
+                        <i className="fa fa-check-square-o"></i> Save
+                      </button>
+                      <button
+                        type="button"
+                        className="btn btn-warning"
+                        onClick={onClick}
+                      >
+                        <i className="ft-x"></i> Cancel
+                      </button>
+                    </div>
+                  </div>
+                  {errorMessage ? (
+                    <div
+                      className="mt-2 alert alert-danger border-0 mb-2"
+                      role="alert"
+                    >
+                      <strong>Error: </strong>
+                      {errorMessage}
+                    </div>
+                  ) : null}
+                </div>
+              )}
+            </div>
           </div>
         </div>
-      </div>
+
+        <style jsx>{`
+          .form-control-custom-2 {
+            height: 48px !important;
+          }
+        `}</style>
+      </React.Fragment>
     );
   }
 }
