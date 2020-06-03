8c56dd5af826bcc57feb58857d48bc75e6cbf6a6
Thanh Quy Vo <vtquy98@gmail.com>
2020-06-04 00:47:56 +0700

    ref: update infura api

--- server/graphql/libs/web3.js
+++ server/graphql/libs/web3.js
@@ -2,11 +2,12 @@ import Web3 from 'web3';
 require('dotenv').config({
   path: './.env'
 });
+// https://ropsten.infura.io/v3/998063e97b9845389fea82d03592512a
 
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const provider = new HDWalletProvider(
   process.env.SEED_WORD,
-  'https://ropsten.infura.io/v3/998063e97b9845389fea82d03592512a'
+  'https://ropsten.infura.io/v3/3b8f30848f594e238c2bf081c0c0908d'
 );
 const web3 = new Web3(provider);
 
